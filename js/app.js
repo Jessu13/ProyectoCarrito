3f6b2e7b29f06b0c4c57691a27125c6488755b05
Jessuar Hoyos <jessuarhandan@gmail.com>
2022-08-29 19:50:50 -0500

    Se crea el proyecto

---
+++ js/app.js
@@ -0,0 +1,54 @@
+//Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners(){
+    // Cuando agregas un curso presionando "Agregar al Carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+// Funciones
+function agregarCurso(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const curso = e.target.parentElement.parentElement;
+        // Enviamos el curso seleccionado para tomar sus datos
+        leerDatosCurso(curso);
+   }
+}
+
+// Lee los datos del curso
+function leerDatosCurso(curso) {
+    const infoCurso = {
+         imagen: curso.querySelector('img').src,
+         titulo: curso.querySelector('h4').textContent,
+         precio: curso.querySelector('.precio span').textContent,
+         id: curso.querySelector('a').getAttribute('data-id'), 
+         cantidad: 1
+    }
+
+
+    if( articulosCarrito.some( curso => curso.id === infoCurso.id ) ) { 
+         const cursos = articulosCarrito.map( curso => {
+              if( curso.id === infoCurso.id ) {
+                   curso.cantidad++;
+                    return curso;
+               } else {
+                    return curso;
+            }
+         })
+         articulosCarrito = [...cursos];
+    }  else {
+         articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+    // console.log(articulosCarrito)
+
+    
+
+    // console.log(articulosCarrito)
+    carritoHTML();
+}
